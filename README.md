fce147e5ef1909b23503ad2bb81cdd3777522e00
Saul Zamarripa Rivera <saul.zamarripa.rivera@gmail.com>
2025-01-29 00:31:57 -0500

    Does not work.

---
+++ README.md
@@ -0,0 +1,4 @@
+# To-Do App
+
+[gtk-rs book](https://gtk-rs.org/gtk4-rs/stable/latest/book/)
+
---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    glib_build_tools::compile_resources(
+        &["src/resources"],
+        "src/resources/resources.gresource.xml",
+        "todo.gresource",
+    );
+}
+
