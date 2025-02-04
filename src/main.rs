e4af5abf46082d04cefe17bc1dc440f85ea2a0d6
Saul Zamarripa Rivera <saul.zamarripa.rivera@gmail.com>
2025-02-04 00:35:05 -0500

    -4) stopped caring 20 commits ago

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ mod task_row;
 mod utils;
 mod window;
 
-use gdk::Display;
 use gtk::prelude::*;
-use gtk::{gdk, gio, glib, Application, CssProvider};
+use gtk::{gio, glib, Application};
 use window::Window;
 
 const APP_ID: &str = "org.gtk_rs.Todo1";
@@ -17,13 +16,8 @@ fn main() -> glib::ExitCode {
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
-    // ANCHOR: connect_startup
     // Connect to signals
-    app.connect_startup(|app| {
-        setup_shortcuts(app);
-        load_css()
-    });
-    // ANCHOR_END: connect_startup
+    app.connect_startup(setup_shortcuts);
     app.connect_activate(build_ui);
 
     // Run the application
@@ -35,21 +29,6 @@ fn setup_shortcuts(app: &Application) {
     app.set_accels_for_action("win.filter('Open')", &["<Ctrl>o"]);
     app.set_accels_for_action("win.filter('Done')", &["<Ctrl>d"]);
 }
-
-// ANCHOR: load_css
-fn load_css() {
-    // Load the CSS file and add it to the provider
-    let provider = CssProvider::new();
-    provider.load_from_resource("/org/gtk_rs/Todo1/style.css");
-
-    // Add the provider to the default screen
-    gtk::style_context_add_provider_for_display(
-        &Display::default().expect("Could not connect to a display."),
-        &provider,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
-}
-// ANCHOR_END: load_css
 
 fn build_ui(app: &Application) {
     // Create a new custom window and present it
